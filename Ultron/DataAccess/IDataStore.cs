de1755d0a09536995e0a5aa677f7fbf871104445
Michael Ibarra <mibarra@aimconsulting.com>
2016-05-31 17:38:19 -0700

    Added generic interface for database operations

---
+++ Ultron/DataAccess/IDataStore.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Ultron.DataAccess
+{
+    public interface IDataStore<T>
+    {
+        IEnumerable<T> Fetch();
+        T Fetch(int id);
+        T Save(T entity);
+        T Delete(T entity);
+    }
+}
